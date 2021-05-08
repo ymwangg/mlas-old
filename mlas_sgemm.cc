2e11f14db1997ff0c1827e2c252fdf165161a562
Ubuntu <ubuntu@ip-172-31-20-143.ec2.internal>
2021-05-08 04:54:11 +0000

    Working

--- mlas_sgemm.cc
+++ mlas_sgemm.cc
@@ -9,6 +9,134 @@
 
 #include "mlas.h"
 
+#include <stdio.h>
+#include <memory.h>
+#include <algorithm>
+#include <limits>
+#include <memory>
+#include <random>
+#include <sstream>
+#if defined(_WIN32)
+#include <windows.h>
+#else
+#include <sys/mman.h>
+#endif
+template <typename T>
+class MatrixGuardBuffer {
+ public:
+  MatrixGuardBuffer() {
+    _BaseBuffer = nullptr;
+    _BaseBufferSize = 0;
+    _ElementsAllocated = 0;
+  }
+
+  ~MatrixGuardBuffer(void) {
+    ReleaseBuffer();
+  }
+
+  T* GetBuffer(size_t Elements, bool ZeroFill = false) {
+    //
+    // Check if the internal buffer needs to be reallocated.
+    //
+
+    if (Elements > _ElementsAllocated) {
+      ReleaseBuffer();
+
+      //
+      // Reserve a virtual address range for the allocation plus an unmapped
+      // guard region.
+      //
+
+      constexpr size_t BufferAlignment = 64 * 1024;
+      constexpr size_t GuardPadding = 256 * 1024;
+
+      size_t BytesToAllocate = ((Elements * sizeof(T)) + BufferAlignment - 1) & ~(BufferAlignment - 1);
+
+      _BaseBufferSize = BytesToAllocate + GuardPadding;
+
+#if defined(_WIN32)
+      _BaseBuffer = VirtualAlloc(NULL, _BaseBufferSize, MEM_RESERVE, PAGE_NOACCESS);
+#else
+      _BaseBuffer = mmap(0, _BaseBufferSize, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#endif
+
+      if (_BaseBuffer == nullptr) {
+        abort();
+      }
+
+      //
+      // Commit the number of bytes for the allocation leaving the upper
+      // guard region as unmapped.
+      //
+
+#if defined(_WIN32)
+      if (VirtualAlloc(_BaseBuffer, BytesToAllocate, MEM_COMMIT, PAGE_READWRITE) == nullptr) {
+        ORT_THROW_EX(std::bad_alloc);
+      }
+#else
+      if (mprotect(_BaseBuffer, BytesToAllocate, PROT_READ | PROT_WRITE) != 0) {
+        abort();
+      }
+#endif
+
+      _ElementsAllocated = BytesToAllocate / sizeof(T);
+      _GuardAddress = (T*)((unsigned char*)_BaseBuffer + BytesToAllocate);
+    }
+
+    //
+    //
+    //
+
+    T* GuardAddress = _GuardAddress;
+    T* buffer = GuardAddress - Elements;
+
+    if (ZeroFill) {
+      std::fill_n(buffer, Elements, T(0));
+
+    } else {
+      const int MinimumFillValue = -23;
+      const int MaximumFillValue = 23;
+
+      int FillValue = MinimumFillValue;
+      T* FillAddress = buffer;
+
+      while (FillAddress < GuardAddress) {
+        *FillAddress++ = (T)FillValue;
+
+        FillValue++;
+
+        if (FillValue > MaximumFillValue) {
+          FillValue = MinimumFillValue;
+        }
+      }
+    }
+
+    return buffer;
+  }
+
+  void ReleaseBuffer(void) {
+    if (_BaseBuffer != nullptr) {
+#if defined(_WIN32)
+      VirtualFree(_BaseBuffer, 0, MEM_RELEASE);
+#else
+      munmap(_BaseBuffer, _BaseBufferSize);
+#endif
+
+      _BaseBuffer = nullptr;
+      _BaseBufferSize = 0;
+    }
+
+    _ElementsAllocated = 0;
+  }
+
+ private:
+  size_t _ElementsAllocated;
+  void* _BaseBuffer;
+  size_t _BaseBufferSize;
+  T* _GuardAddress;
+};
+
+
 inline void
 CPUCacheFlushImpl(const char* addr, unsigned int len)
 {
@@ -58,19 +186,22 @@ SGEMM(bool pack_b,
         size_t pack_b_size = MlasGemmPackBSize(N, K);
         printf("pack_b_size=%ld\n", pack_b_size);
         std::vector<float> B_packed(pack_b_size);
-        MlasGemmPackB(CblasNoTrans, N, K, B.data(), N, B_packed.data());
+        MatrixGuardBuffer<uint8_t> BufferBPacked;
+        void* PackedB = BufferBPacked.GetBuffer(pack_b_size, true);
+
+        MlasGemmPackB(CblasNoTrans, N, K, B.data(), N, PackedB);
 
         MlasGemm(trans_a ? CblasTrans : CblasNoTrans, static_cast<size_t>(M),
                  static_cast<size_t>(N), static_cast<size_t>(K), alpha, A.data(), trans_a ? M : K,
-                 B_packed.data(), beta, C.data(), N);
+                 PackedB, beta, C.data(), N);
 
         for (int i = 0; i < iter; i++) {
             CPUCacheFlushImpl((char*)A.data(), M * K * sizeof(float));
-            CPUCacheFlushImpl((char*)B.data(), K * N * sizeof(float));
+            CPUCacheFlushImpl((char*)PackedB, pack_b_size);
             auto t1 = std::chrono::high_resolution_clock::now();
             MlasGemm(trans_a ? CblasTrans : CblasNoTrans, static_cast<size_t>(M),
                      static_cast<size_t>(N), static_cast<size_t>(K), alpha, A.data(),
-                     trans_a ? M : K, B_packed.data(), beta, C.data(), N);
+                     trans_a ? M : K, PackedB, beta, C.data(), N);
             auto t2 = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> dt = t2 - t1;
             tsum += dt.count();
@@ -108,6 +239,6 @@ main(int argc, char** argv)
     //     do_flush = argv[4];
     // }
 
-    SGEMM(true, false, false, 64, 3072, 768);
+    SGEMM(true, false, false, 64, 768, 3072);
     return 0;
 }
